cc00d70beebd8f6e7e4bef2892b7a6d28315c6be
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-03-20 16:48:18 +0900

    2-5. 웹팩으로 빌드하기

--- lecture/webpack.config.js
+++ lecture/webpack.config.js
@@ -12,6 +12,17 @@ module.exports = {
         app: ['./client'],
     }, // 입력
 
+    module: {
+        rules: [{
+           test: /\.jsx?/,
+           loader: 'babel-loader',
+           options: {
+               presets: ['@babel/preset-env', '@babel/preset-react'],
+               plugins: ['@babel/plugin-proposal-class-properties'],
+           },
+        }],
+    },
+
     output: {
         path: path.join(__dirname, 'dist'), // C:\Users\jyryu\WebstormProjects\practice-react\lecture\dist
         filename: 'app.js'
