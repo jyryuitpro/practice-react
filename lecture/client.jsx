779cb3cc2d0eb8fa14a342a86e9580d331e29d60
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-03-20 16:05:55 +0900

    2-4. 모듈 시스템과 웹팩 설정

--- lecture/client.jsx
+++ lecture/client.jsx
@@ -1,4 +1,6 @@
 const React = require('react');
 const ReactDom = require('react-dom');
 
-ReactDom.render(<GuGuDan />);
+const WordRelay = require('./WordRelay');
+
+ReactDom.render(<WordRelay />, document.querySelector('#root'));
--- lecture/webpack.config.js
+++ lecture/webpack.config.js
@@ -1,3 +1,19 @@
-module.exports = {
+const path = require('path');
 
+module.exports = {
+    name: 'wordrelay-setting',
+    mode: 'development', // 실서비스 : production
+    devtool: 'eval',
+    resolve: {
+      extensions: ['.js', '.jsx']
+    },
+
+    entry: {
+        app: ['./client'],
+    }, // 입력
+
+    output: {
+        path: path.join(__dirname, 'dist'), // C:\Users\jyryu\WebstormProjects\practice-react\lecture\dist
+        filename: 'app.js'
+    }, // 출력
 };
