1ebe95a2a2a2afd2759ba434620a5f48273c9aa9
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-03-21 15:15:47 +0900

    2-7. @babel/preset-env와 plugins

--- 01.구구단/webpack.config.js
+++ 01.구구단/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
     mode: 'development', // 실서비스 : production
@@ -16,12 +17,22 @@ module.exports = {
             test: /\.jsx?$/,
             loader: 'babel-loader',
             options: {
-                presets: ['@babel/preset-env', '@babel/preset-react'],
+                presets: [
+                    ['@babel/preset-env', {
+                        targets: {
+                          browsers: ['> 1% in KR', 'last 2 chrome versions'], // browserslist
+                        },
+                        debug: true,
+                    }],
+                    '@babel/preset-react',
+                ],
                 plugins: [],
             },
         }],
     },
-
+    plugins: [
+        new webpack.LoaderOptionsPlugin({ debug: true }),
+    ],
     output: {
         path: path.join(__dirname, 'dist'), // C:\Users\jyryu\WebstormProjects\practice-react\lecture\dist
         filename: 'app.js'
