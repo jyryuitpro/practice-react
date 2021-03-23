83881ade35c03c5142542a190443cdf97e0d39cd
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-03-24 00:07:10 +0900

    3-1. import와 require 비교

---
+++ lecture/NumberBaseball.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class NumberBaseball extends Component {
+
+}
+
+export const hello = 'hello'; // import { hello }
+export const bye = 'hello'; // import { hello, bye }
+
+export default NumberBaseball; // import NumberBaseball
+
+// const React = require('react');
+// exports.hello = 'hello';
+// module.exports = NumberBaseball;
