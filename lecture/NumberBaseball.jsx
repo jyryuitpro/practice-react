c6677c7e4d424bfdc3ae09cd61e6a24507406572
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-03-27 09:53:03 +0900

    3-7. Q&A

--- lecture/NumberBaseball.jsx
+++ lecture/NumberBaseball.jsx
@@ -1,29 +1,34 @@
 import React, {Component} from 'react';
 import Try from './Try';
 
-function getNumbers() { // 숫자 네 개를 겹치지 않고 랜덤하게 뽑는 함수
-    const candidate = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    const array = [];
-    for (let i = 0; i < 4; i += 1) {
-        const chosen = candidate.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
-        array.push(chosen);
-    }
-    return array;
-}
+// function getNumbers() { // 숫자 네 개를 겹치지 않고 랜덤하게 뽑는 함수
+//
+// }
 class NumberBaseball extends Component {
+    getNumbers = () => {
+        const candidate = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        const array = [];
+        for (let i = 0; i < 4; i += 1) {
+            const chosen = candidate.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+            array.push(chosen);
+        }
+        return array;
+    }
+
     state = {
         result: '',
         value: '',
-        answer: getNumbers(),
+        answer: this.getNumbers(),
         tries: [], // push 쓰면 안 돼요
     };
 
     onSubmitForm = (e) => {
+        const {result, value, tries, answer} = this.state;
         e.preventDefault();
-        if (this.state.value === this.state.answer.join('')) {
+        if (answer.join('') === value) {
             this.setState({
                 result: '홈런',
-                tries: [...this.state.tries, {try: this.state.value, result: '홈런!'}]
+                tries: [...tries, {try: value, result: '홈런!'}]
             });
             alert('게임을 다시 시작합니다!');
             this.setState({
@@ -32,12 +37,12 @@ class NumberBaseball extends Component {
                 tries: [],
             });
         } else {
-            const answerArray = this.state.value.split('').map((v) => parseInt(v));
+            const answerArray = value.split('').map((v) => parseInt(v));
             let strike = 0;
             let ball = 0;
-            if (this.state.tries.length >= 9) {
+            if (tries.length >= 9) {
                 this.setState({
-                    result: `10번 넘게 틀려서 실패! 답은 ${this.state.answer.join(',')}였습니다!`,
+                    result: `10번 넘게 틀려서 실패! 답은 ${answer.join(',')}였습니다!`,
                 });
                 alert('게임을 다시 시작합니다!');
                 this.setState({
@@ -47,19 +52,19 @@ class NumberBaseball extends Component {
                 });
             } else {
                 for (let i = 0; i < 4; i += 1) {
-                    if (answerArray[i] === this.state.answer[i]) {
+                    if (answer[i] === answerArray[i]) {
                         strike += 1;
-                    } else if (this.state.answer.includes(answerArray[i])) {
+                    } else if (answer.includes(answerArray[i])) {
                         ball += 1;
                     }
                 }
                 this.setState({
-                    tries: [...this.state.tries, {try: this.state.value, result: `${strike} 스트라이크, ${ball} 볼입니다`}],
+                    tries: [...tries, {try: value, result: `${strike} 스트라이크, ${ball} 볼입니다`}],
                     value: '',
                 });
             }
         }
-        console.log(this.state.value);
+        console.log(value);
     };
 
     onChangeInput = (e) => {
@@ -70,15 +75,16 @@ class NumberBaseball extends Component {
     };
 
     render() {
+        const {result, value, tries} = this.state;
         return (
             <>
-                <h1>{this.state.result}</h1>
+                <h1>{result}</h1>
                 <form onSubmit={this.onSubmitForm}>
-                    <input maxLength={4} value={this.state.value} onChange={this.onChangeInput}/>
+                    <input value={value} maxLength={4} onChange={this.onChangeInput}/>
                 </form>
-                <div>시도: {this.state.tries.length}</div>
+                <div>시도: {tries.length}</div>
                 <ul>
-                    {this.state.tries.map((v, i) => {
+                    {tries.map((v, i) => {
                         return ( // return 생략가능
                             <Try key={`${i + 1}차 시도 :`} tryInfo={v} />
                         );
