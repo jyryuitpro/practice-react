d9c87261f715482dff457135ee83c45add63a9af
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-03-28 22:43:07 +0900

    4-1. React 조건문

---
+++ lecture/ResponseCheck.jsx
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+
+class ResponseCheck extends Component {
+    state = {
+        state: 'waiting',
+        message: '클릭해서 시작하세요.',
+        result: [],
+    };
+
+    onClickScreen = () => {
+
+    };
+
+    renderAverage = () => {
+        const {result} = this.state;
+        return result.length === 0 ? null : <div>평균시간: {result.reduce((a, c) => a + c) / result.length}ms</div>
+    };
+
+    render() {
+        const {state, message} = this.state;
+        return (
+            <>
+                <div id="screen" className={state} onClick={this.onClickScreen}>{message}</div>
+                {this.renderAverage()}
+            </>
+        );
+    }
+}
+
+export default ResponseCheck;
--- lecture/client.jsx
+++ lecture/client.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import {hot} from 'react-hot-loader/root';
 
-import NumberBaseball from './NumberBaseball';
+// import NumberBaseball from './NumberBaseball';
 // import NumberBaseball from './RenderTest';
+import ResponseCheck from './ResponseCheck';
 
-const Hot = hot(NumberBaseball);
+const Hot = hot(ResponseCheck);
 
 ReactDom.render(<Hot />, document.querySelector('#root'));
