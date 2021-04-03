07148271a9de468a4a95649d39be8f60e7480445
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-04-04 00:10:43 +0900

    2-4. 모듈 시스템과 웹팩 설정

--- 02.끝말잇기/webpack.config.js
+++ 02.끝말잇기/webpack.config.js
@@ -1,3 +1,18 @@
-module.exports = {
+const path = require('path');
 
+module.exports = {
+    name: 'word-relay-setting',
+    mode: 'development', // 실서비스: production
+    devtool: 'eval',
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+
+    entry: { // 입력
+        app: ['./client'],
+    },
+    output: { // 출력
+        path: path.join(__dirname, 'dist'),
+        filename: 'app.js',
+    },
 };
