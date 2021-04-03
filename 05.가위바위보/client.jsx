dac8b1971bf9002bc1cd3f0e7319bfd39b987ed6
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-04-03 16:53:29 +0900

    1-1. 리액트를 왜 쓰는가 && 1-2. 첫 리액트 컴포넌트

--- 05.가위바위보/client.jsx
+++
@@ -1,8 +0,0 @@
-const React = require('react');
-const ReactDom = require('react-dom');
-
-const RSP = require('./RSP');
-
-const Hot = hot(RSP);
-
-ReactDom.render(<Hot/>, document.querySelector('#root'));
--- 10.Example/client.jsx
+++
@@ -1,6 +0,0 @@
-const React = require('react');
-const ReactDom = require('react-dom');
-
-const Example = require('./Example');
-
-ReactDom.render(<Example/>, document.querySelector('#root'));
