01f9993c9be8c251416d7ed33022bf0ed300b61b
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-04-04 21:43:44 +0900

    3-2. 리액트 반복문(map)

--- 03.숫자야구/NumberBaseballClass.jsx
+++ 03.숫자야구/NumberBaseballClass.jsx
@@ -2,8 +2,44 @@
 const React = require('react');
 const {Component} = React;
 
-class NumberBaseballClass extends Component {
+function getNumbers() { // 숫자 네 개를 겹치지 않고 랜덤하게 뽑는 함수
 
+}
+
+class NumberBaseballClass extends Component {
+    state = {
+        result: '',
+        value: '',
+        answer: getNumbers(),
+        tries: [],
+    };
+
+    onSubmitForm = () => {
+
+    };
+
+    onChangeInput = () => {
+
+    };
+
+    render() {
+        return (
+            <>
+                <h1>{this.state.result}</h1>
+                <form onSubmit={this.onSubmitForm}>
+                    <input maxLength={4} value={this.state.value} onChange={this.onChangeInput}/>
+                </form>
+                <div>시도: {this.state.tries.length}</div>
+                <ul>
+                    {['사과', '바나나', '포도', '귤', '감', '배', '밤'].map((v) => {
+                        return (
+                            <li>{v}</li>
+                        );
+                    })}
+                </ul>
+            </>
+        );
+    }
 }
 
 // export const hello = 'hello'; // import {hello}
