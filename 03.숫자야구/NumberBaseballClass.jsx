5afc4282372edaae5ac4f39353061647061bfeda
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-04-11 13:23:59 +0900

    3-7. Q&A

--- 03.숫자야구/NumberBaseballClass.jsx
+++ 03.숫자야구/NumberBaseballClass.jsx
@@ -20,11 +20,12 @@ class NumberBaseballClass extends Component {
     };
 
     onSubmitForm = (e) => {
+        const {result, value, tries, answer} = this.state;
         e.preventDefault();
-        if (this.state.value === this.state.answer.join('')) {
+        if (value === answer.join('')) {
             this.setState({
                 result: '홈런!',
-                tries: [...this.state.tries, {try: this.state.value, result: '홈런!'}],
+                tries: [...tries, {try: value, result: '홈런!'}],
             });
             alert('게임을 다시 시작합니다!');
             this.setState({
@@ -33,12 +34,12 @@ class NumberBaseballClass extends Component {
                 tries: [],
             });
         } else {
-            const answerArray = this.state.value.split('').map((v) => parseInt(v));
+            const answerArray = value.split('').map((v) => parseInt(v));
             let strike = 0;
             let ball = 0;
-            if (this.state.tries.length > 9) {
+            if (tries.length > 9) {
                 this.setState({
-                    result: `10번 넘게 틀려서 실패! 답은 ${this.state.answer.join(',')}였습니다!`,
+                    result: `10번 넘게 틀려서 실패! 답은 ${answer.join(',')}였습니다!`,
                 });
                 alert('게임을 다시 시작합니다!');
                 this.setState({
@@ -48,14 +49,14 @@ class NumberBaseballClass extends Component {
                 });
             } else {
                 for (let i = 0; i < 4; i += 1) {
-                    if (answerArray[i] === this.state.answer[i]) {
+                    if (answerArray[i] === answer[i]) {
                         strike +=1;
-                    } else if (this.state.answer.includes(answerArray[i])) {
+                    } else if (answer.includes(answerArray[i])) {
                         ball +=1;
                     }
                 }
                 this.setState({
-                    tries: [...this.state.tries, {try: this.state.value, result: `${strike} 스트라이크, ${ball} 볼입니다`}],
+                    tries: [...tries, {try: value, result: `${strike} 스트라이크, ${ball} 볼입니다`}],
                     value: '',
                 });
             }
@@ -70,15 +71,16 @@ class NumberBaseballClass extends Component {
     };
 
     render() {
+        const {result, value, tries} = this.state;
         return (
             <>
-                <h1>{this.state.result}</h1>
+                <h1>{result}</h1>
                 <form onSubmit={this.onSubmitForm}>
-                    <input maxLength={4} value={this.state.value} onChange={this.onChangeInput}/>
+                    <input maxLength={4} value={value} onChange={this.onChangeInput}/>
                 </form>
-                <div>시도: {this.state.tries.length}</div>
+                <div>시도: {tries.length}</div>
                 <ul>
-                    {this.state.tries.map((v, i) => { // 화살표 함수 return 생략가능
+                    {tries.map((v, i) => { // 화살표 함수 return 생략가능
                         return (
                             <Try key={`${i + 1}차 시도 :`} tryInfo={v}/>
                         );
--- 03.숫자야구/Try.jsx
+++ 03.숫자야구/Try.jsx
@@ -2,10 +2,11 @@ import React, {Component} from 'react';
 
 class Try extends Component {
     render() {
+        const {tryInfo} = this.props;
         return (
             <li>
-                <div>{this.props.tryInfo.try}</div>
-                <div>{this.props.tryInfo.result}</div>
+                <div>{tryInfo.try}</div>
+                <div>{tryInfo.result}</div>
             </li>
         );
     }
