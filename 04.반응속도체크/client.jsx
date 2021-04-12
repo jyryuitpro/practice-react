27b4bf485dbcf6f91b72c20ea0d434488de71ea3
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-04-12 18:38:52 +0900

    4-1. React 조건문

---
+++ 04.반응속도체크/client.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import ResponseCheckClass from './ResponseCheckClass';
+
+ReactDom.render(<ResponseCheckClass/>, document.querySelector('#root'));
