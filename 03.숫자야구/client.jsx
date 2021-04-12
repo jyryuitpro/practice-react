d576f681432f1cbee65e82223dd0e867ed4a7a2a
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-04-12 10:25:39 +0900

    3-10. shouldComponentUpdate

--- 03.숫자야구/client.jsx
+++ 03.숫자야구/client.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 // import NumberBaseballClass from './NumberBaseballClass';
-import NumberBaseballHooks from './NumberBaseballHooks';
+// import NumberBaseballHooks from './NumberBaseballHooks';
+import RenderTest from './RenderTest';
 
-ReactDom.render(<NumberBaseballHooks/>, document.querySelector('#root'));
+ReactDom.render(<RenderTest/>, document.querySelector('#root'));
