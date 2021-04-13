ffbba6f1f9143da4a34ba02286d010ba0f203ac0
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-04-13 21:56:19 +0900

    5-1. 리액트 라이프사이클 소개

---
+++ 05.가위바위보/client.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import RSPClass from './RSPClass';
+
+ReactDom.render(<RSPClass/>, document.querySelector('#root'));
---
+++ 05.가위바위보/webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+// process.env.NODE_ENV = 'production';
+
+module.exports = {
+    name: 'rsp-setting',
+    mode: 'development', // 실서비스: production
+    devtool: 'eval', // 실서비스: hidden-source-map
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+
+    entry: { // 입력
+        app: ['./client'],
+    },
+    module: { // 모듈적용
+        rules: [{
+            test: /\.jsx?$/,
+            loader: 'babel-loader',
+            options: {
+                presets: [
+                    ['@babel/preset-env', {
+                        targets: {
+                            browsers: ['> 1% in KR'],
+                        },
+                        debug: true,
+                    }],
+                    '@babel/preset-react',
+                ],
+                plugins: [
+                    '@babel/plugin-proposal-class-properties',
+                    'react-refresh/babel'
+                ],
+            },
+        }],
+    },
+    plugins: [
+        new RefreshWebpackPlugin(),
+    ],
+    output: { // 출력
+        path: path.join(__dirname, 'dist'),
+        filename: 'app.js',
+        publicPath: '/dist/', // app.use('/dist', express.static(__dirname, 'dist'));
+    },
+    devServer: {
+        publicPath: '/dist/',
+        hot: true,
+    },
+};
