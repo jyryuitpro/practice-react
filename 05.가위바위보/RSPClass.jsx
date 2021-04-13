256c77be4ea69b61093a143ebb855b9829b91a62
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-04-13 22:46:24 +0900

    5-2. setInterval과 라이프사이클 연동하기

--- 05.가위바위보/RSPClass.jsx
+++ 05.가위바위보/RSPClass.jsx
@@ -3,28 +3,62 @@ import React, {Component} from 'react';
 // 클래스의 경우 -> constructor -> render -> ref -> componentDidMount
 // setState/props 바뀔때 -> shouldComponentUpdate(true) -> render -> componentDidUpdate
 // 부모가 나를 없앴을 때 -> componentWillUnmount -> 소멸
+const rspCoords = {
+    바위: '0',
+    가위: '-142px',
+    보: '-284px',
+};
+
+const scores = {
+    가위: 1,
+    바위: 0,
+    보: -1,
+};
+
 class RSPClass extends Component {
     state = {
         result: '',
-        imgCoord: 0,
+        imgCoord: '0',
         score: 0,
     };
 
-    componentDidMount() { // 컴포넌트가 첫 렌더링된 후
+    interval;
 
+    componentDidMount() { // 컴포넌트가 첫 렌더링된 후, 여기에 비동기 요청을 많이 해요.
+        this.interval = setInterval(() => { // 비동기 함수 바깥에 있는 변수를 참조하면 클로저가 발생해요.
+            const {imgCoord} = this.state;
+            // console.log(imgCoord === rspCoords.바위);
+            if (imgCoord === rspCoords.바위) {
+                this.setState({
+                    imgCoord: rspCoords.가위,
+                });
+            } else if (imgCoord === rspCoords.가위) {
+                this.setState({
+                    imgCoord: rspCoords.보,
+                });
+            } else if (imgCoord === rspCoords.보) {
+                this.setState({
+                    imgCoord: rspCoords.바위,
+                });
+            }
+        }, 1000);
     }
 
-    shouldComponentUpdate(nextProps, nextState, nextContext) {
-        return true;
-    }
+    // shouldComponentUpdate(nextProps, nextState, nextContext) {
+    //     return true;
+    // }
 
     componentDidUpdate(prevProps, prevState, snapshot) { // 리렌더링 후
 
     }
 
-    componentWillUnmount() { // 컴포넌트가 제거되기 직전
-
+    componentWillUnmount() { // 컴포넌트가 제거되기 직전, 비동기 요청 정리를 많이 해요
+        clearInterval(this.interval);
     }
+
+    onClickBtn = (choice) => {
+
+    };
 
     render() {
         const {result, imgCoord, score} = this.state;
@@ -32,9 +66,9 @@ class RSPClass extends Component {
             <>
                 <div id="computer" style={{background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0`}}></div>
                 <div>
-                    <button id="rock" className="btn" onClick={() => onClickBtn('바위')}>바위</button>
-                    <button id="scissor" className="btn" onClick={() => onClickBtn('가위')}>가위</button>
-                    <button id="paper" className="btn" onClick={() => onClickBtn('보')}>보</button>
+                    <button id="rock" className="btn" onClick={() => this.onClickBtn('바위')}>바위</button>
+                    <button id="scissor" className="btn" onClick={() => this.onClickBtn('가위')}>가위</button>
+                    <button id="paper" className="btn" onClick={() => this.onClickBtn('보')}>보</button>
                 </div>
                 <div>{result}</div>
                 <div>현재 {score}점</div>
