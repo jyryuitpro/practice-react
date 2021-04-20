7857254eb19b7b4ad7e0ae342cd0c1c7a69d43a8
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-04-20 23:44:08 +0900

    6-5. useMemo와 useCallback

--- 06.로또 추첨기/LottoHooks.jsx
+++ 06.로또 추첨기/LottoHooks.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useRef, useEffect} from 'react';
+import React, {useState, useRef, useEffect, useMemo, useCallback} from 'react';
 import BallFunction from "./BallFunction";
 
 function getWinNumbers() {
@@ -14,13 +14,17 @@ function getWinNumbers() {
 }
 
 const LottoHooks = () => {
-    const [winNumbers, setWinNumbers] = useState(getWinNumbers());
+    // useMemo : 복잡한 함수 결괏값(리턴값)을 기억
     const [winBalls, setWinBalls] = useState([]);
+    const lottoNumbers = useMemo(() => getWinNumbers(), [winNumbers]);
+    const [winNumbers, setWinNumbers] = useState(lottoNumbers);
     const [bonus, setBonus] = useState(null);
     const [redo, setRedo] = useState(false);
+    // useRef : 일반 값을 기억
     const timeouts = useRef([]);
 
     useEffect(() => {
+        console.log('useEffect');
         for (let i = 0; i < winNumbers.length - 1; i++) {
             timeouts.current[i] = setTimeout(() => {
                 setWinBalls((prevBalls) => [...prevBalls, winNumbers[i]]);
@@ -38,13 +42,16 @@ const LottoHooks = () => {
     }, [timeouts.current]); // input 자리가 빈 배열이면 componentDidMount와 동일
     // input 자리에 배열에 요소가 있으면 componentDidMount랑 componentDidUpdate 둘 다 수행
 
-    const onClickRedo = () => {
+    // useCallback : 함수를 기억
+    const onClickRedo = useCallback(() => {
+        console.log('onClickRedo');
+        console.log(winNumbers);
         setWinNumbers(getWinNumbers());
         setWinBalls([]);
         setBonus(null);
         setRedo(false);
         timeouts.current = [];
-    };
+    }, [winNumbers]);
 
     return (
         <>
@@ -53,7 +60,7 @@ const LottoHooks = () => {
                 {winBalls.map((v) => <BallFunction key={v} number={v} />)}
             </div>
             <div>보너스</div>
-            {bonus && <BallFunction number={bonus} />}
+            {bonus && <BallFunction number={bonus} onClick={onClickRedo}/>}
             {redo && <button onClick={onClickRedo}>한 번 더!</button>}
         </>
     );
